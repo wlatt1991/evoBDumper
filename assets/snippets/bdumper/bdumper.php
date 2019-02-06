aeec297e091f0c4e832889d4e3eb5c98decd8fde
wlatt1991 <wlatt1991@yandex.ru>
2019-02-07 01:03:35 +0500

    fix

--- assets/snippets/bdumper/bdumper.php
+++ assets/snippets/bdumper/bdumper.php
@@ -4,25 +4,25 @@ require MODX_BASE_PATH . 'assets/snippets/bdumper/class_mysqldumper.php';
 global $path;
 $modx = evolutionCMS();
 
-// function snapshot(&$dumpstring)
-// {
-//     global $path;
-//     file_put_contents($path, $dumpstring, FILE_APPEND);
-//     return true;
-// }
+function snapshot(&$dumpstring)
+{
+    global $path;
+    file_put_contents($path, $dumpstring, FILE_APPEND);
+    return true;
+}
 
-// function parsePlaceholder($tpl = '', $ph = array())
-// {
-//     if (empty($ph) || empty($tpl)) {
-//         return $tpl;
-//     }
+function parsePlaceholder($tpl = '', $ph = array())
+{
+    if (empty($ph) || empty($tpl)) {
+        return $tpl;
+    }
 
-//     foreach ($ph as $k => $v) {
-//         $k = "[+{$k}+]";
-//         $tpl = str_replace($k, $v, $tpl);
-//     }
-//     return $tpl;
-// }
+    foreach ($ph as $k => $v) {
+        $k = "[+{$k}+]";
+        $tpl = str_replace($k, $v, $tpl);
+    }
+    return $tpl;
+}
 
 $modx->config['snapshot_path'] = MODX_BASE_PATH . 'assets/backup/';
 
@@ -41,7 +41,7 @@ if (!is_writable(rtrim($modx->config['snapshot_path'], '/'))) {
     exit;
 }
 
-$today = date('Y-m-d_H-i-s');
+$today = date('Y-m-d_H-i-s_00');
 $path = "{$modx->config['snapshot_path']}{$today}.sql";
 @set_time_limit(120);
 $dbase = trim($dbase, '`');
