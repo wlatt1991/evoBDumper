1826c0fc322bac178d9c74045b7202ea2d817262
wlatt1991 <wlatt1991@yandex.ru>
2019-02-07 01:06:32 +0500

    fix

--- assets/snippets/bdumper/bdumper.php
+++ assets/snippets/bdumper/bdumper.php
@@ -45,7 +45,6 @@ $today = date('Y-m-d_H-i-s_00');
 $path = "{$modx->config['snapshot_path']}{$today}.sql";
 @set_time_limit(120);
 $dbase = trim($dbase, '`');
-print_r($modx);
 $dumper = new Mysqldumper($database_server, $dbase);
 $dumper->setDroptables(true);
 $dumper->createDump('snapshot');
