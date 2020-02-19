a43dad9962dcb1f88dcade231bf8593d4d6566cf
ywx548750 <yurchenko.vladislav@huawei.com>
2020-02-19 14:45:07 +0500

    add plugin

--- assets/snippets/bdumper/class_evobdumper.php
+++ assets/snippets/bdumper/class_evobdumper.php
@@ -71,7 +71,7 @@ class evoBDumper
         $today = date('Y-m-d_H-i-s');
         $path = "{$modx->config['snapshot_path']}evoBDumper_{$today}.sql";
 		file_put_contents($path, $output, FILE_APPEND);
-		return 'snapshot created';
+		//return 'snapshot created';
     }
 
     public function object2Array($obj)
